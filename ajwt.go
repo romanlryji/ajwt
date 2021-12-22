cb805aab62e86ec19f391e2aee9177e785a657b7
romanlryji <romanlryji@yahoo.co.uk>
2021-12-22 09:49:41 +0300

    ParseToken

--- ajwt.go
+++ ajwt.go
@@ -1,6 +1,7 @@
 package ajwt
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt"
@@ -25,4 +26,30 @@ func GenerateJWT(d time.Duration, signingKey []byte, cl map[string]interface{}) 
 	}
 
 	return tokenString, nil
+}
+
+type TokenClaims struct {
+	jwt.StandardClaims
+	Login string
+	Email string
+}
+
+func ParseToken(accessToken string, signingKey string) (TokenClaims, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &TokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return TokenClaims{}, err
+	}
+
+	claims, ok := token.Claims.(TokenClaims)
+	if !ok {
+		return TokenClaims{}, errors.New("token claims are not of type *tokenClaims")
+	}
+
+	return claims, nil
 }
